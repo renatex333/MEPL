ddd80c83d7a5f258f00e4db89e4282d1f7aff829
renatex333 <renato9302@hotmail.com>
2023-11-24 00:11:24 -0300

    Updating automated script for compiler build

--- script.sh
+++ script.sh
@@ -7,8 +7,7 @@ SOURCE_DIR=$COMPILER_DIR/src
 LEXER_DIR=$SOURCE_DIR/lexer
 PARSER_DIR=$SOURCE_DIR/parser
 SEMANTIC_DIR=$SOURCE_DIR/semantic
-
-# rm "$PARSER_DIR/elegant_parser.tab.c" "$PARSER_DIR/elegant_parser.tab.h" "$LEXER_DIR/lex.yy.c" "$PARSER_DIR/elegant_parser"
+export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR
 
 cd $PARSER_DIR
 read -p "Debug mode? [y/N]: " DEBUG
@@ -20,5 +19,13 @@ fi
 cd $LEXER_DIR
 flex "$LEXER_DIR/elegant_lexer.l"
 cd $PARSER_DIR
-gcc -o "$PARSER_DIR/elegant_parser" "$PARSER_DIR/elegant_parser.tab.c" "$LEXER_DIR/lex.yy.c" -lfl
-"$PARSER_DIR/elegant_parser" < "$EXAMPLE_DIR/elegant_example.mepl"
+gcc -o "$PARSER_DIR/elegant_parser" "$PARSER_DIR/elegant_parser.tab.c" "$LEXER_DIR/lex.yy.c" -lfl -lpython3.10 $(python3.10-config --cflags --ldflags)
+"$PARSER_DIR/elegant_parser" "$EXAMPLE_DIR/elegant_example.mepl"
+if [[ $DEBUG =~ ^[Yy]$ ]]; then
+    nasm -f elf -F dwarf -o "$EXAMPLE_DIR/elegant_example.o" "$EXAMPLE_DIR/elegant_example.asm"
+    gcc -m32 -no-pie -g -o "$EXAMPLE_DIR/elegant_example" "$EXAMPLE_DIR/elegant_example.o"
+else
+    nasm -f elf -o "$EXAMPLE_DIR/elegant_example.o" "$EXAMPLE_DIR/elegant_example.asm"
+    gcc -m32 -no-pie -o "$EXAMPLE_DIR/elegant_example" "$EXAMPLE_DIR/elegant_example.o"
+fi
+"$EXAMPLE_DIR/elegant_example"
